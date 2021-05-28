2f5944091b347c8b2d0fa9a6d2a513fe1c4c1db1
ryeoly <youddbb@naver.com>
2021-05-28 23:07:02 +0900

    21.05.28(금) 득렬 최종!

---
+++ Project3/frontend/src/components/core/search.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+                    <v-layout wrap>
+                        <v-flex xs3 md3>
+
+                        </v-flex>
+                    </v-layout>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "search.vue",
+        data () {
+            return {
+                model: null,
+                product: null,
+                category: null,
+                purpose: null,
+
+                categoriesPurposes: {
+                    'Farm Animals':
+                        ['cow', 'sheep', 'hen'],
+                    'Directions':
+                        ['left', 'right', 'up', 'down'],
+                    'Time':
+                        ['past', 'present', 'future'],
+                    'Element':
+                        ['air', 'water', 'tierra', 'fire']
+                }
+            }
+        },
+        computed: {
+            categories() {
+                return Object.keys(this.categoriesPurposes)
+            },
+            purposes() {
+                if (!this.category) {
+                    return null
+                } else {
+                    return this.categoriesPurposes[this.category]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
